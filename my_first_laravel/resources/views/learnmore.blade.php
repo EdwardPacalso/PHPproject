071fd95c01368eb3408046510d9b074c8ce28a86
EdwardPacalso <pacalsoedward24@gmail.com>
2023-10-18 21:05:32 +0800

    updated nav

---
+++ my_first_laravel/resources/views/learnmore.blade.php
@@ -0,0 +1,1 @@
+welcome to learn more page
--- my_first_laravel/routes/web.php
+++ my_first_laravel/routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('learnmore', function () {
+    return view('learnmore');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('blogs', function () {
+    return view('blogs');
+});
